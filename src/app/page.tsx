b9aeada1945ffa83aeed67b5c14e32b061ed5e81
João Victor <j.victorbahia@gmail.com>
2023-11-12 21:35:14 -0300

    style: Adding the reset CSS and default font of the project.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import HomePage from '@/components/molecules/HomePage'
 import JobForm from '../components/molecules/JobForm/index'
+import '../../src/styles/global.css'
 
 export default function Home() {
   return (
